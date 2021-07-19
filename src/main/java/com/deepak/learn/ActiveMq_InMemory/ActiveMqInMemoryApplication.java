e739c9d8cddedbdcdcb9d232d9ad4d3d3c80745a
Deepak <Deepak@192.168.56.1>
2021-07-20 04:37:59 +0530

    Initial commit

---
+++ src/main/java/com/deepak/learn/ActiveMq_InMemory/ActiveMqInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.learn.ActiveMq_InMemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqInMemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepak/learn/ActiveMq_InMemory/ActiveMqInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepak.learn.ActiveMq_InMemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
